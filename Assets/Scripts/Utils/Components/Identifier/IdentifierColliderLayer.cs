4debb87cff2fe8cbcddc89481c5181061006be3f
João Felipe <joaokecek.business@gmail.com>
2025-07-15 01:14:36 -0300

    Start Interactables
    Start Mover too

--- Assets/Scripts/Utils/Components/Identifier/IdentifierColliderLayer.cs
+++ Assets/Scripts/Utils/Components/Identifier/IdentifierColliderLayer.cs
@@ -9,15 +9,15 @@ namespace KeceK.Utils.Components
         [SerializeField] [FoldoutGroup("References")]
         private Collider2D[] _collidersToChangeCollisionLayer;
 
-        [SerializeField] [FoldoutGroup("Settings")] [Tooltip("If true, the layer will be inverted, meaning Player1 will not touch Player2 Layer")]
-        private bool _isInverted;
+        [SerializeField] [FoldoutGroup("Settings")] [Tooltip("If true, the layer will be inverted, meaning Player1 will not touch Player2 Layer, will touch only Player1 Layer and vice versa.")]
+        private bool _thisPlayerTypeCanCollideWithThisObject;
         
         protected override void Identify(PlayerType playerType)
         {
             foreach (Collider2D collider2D in _collidersToChangeCollisionLayer)
             {
                 int layerToExclude = -1;
-                if (_isInverted)
+                if (_thisPlayerTypeCanCollideWithThisObject)
                 {
                     layerToExclude = playerType == PlayerType.Player1 
                         ? PlayersInfo.Player2LayerMaskIndex 
