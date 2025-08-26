89d51278182be5ad32690f472d08cbd2e02ab826
Joao Kecek <joaokecek.business@gmail.com>
2025-08-26 08:51:56 -0300

    Start Lock Level UI

---
+++ Assets/Scripts/UI/Canvas/LevelLockedCanvas.cs
@@ -0,0 +1,18 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace KeceK.UI
+{
+    public class LevelLockedCanvas : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField _inputField;
+        [SerializeField] private LevelPasswordSO _levelPasswordSO;
+
+
+        private void OnEnable()
+        {
+            // _inputField.texte
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SO/LevelPasswordSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace KeceK.UI
+{
+    [CreateAssetMenu(fileName = "LevelPasswordSO", menuName = "Scriptable Objects/LevelPasswordSO")]
+    public class LevelPasswordSO : ScriptableObject
+    {
+        public string Password;
+    }
+}
