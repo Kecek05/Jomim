5c3a5101c7935ef34c29027480bac2d11c290f09
Joao Kecek <joaokecek.business@gmail.com>
2025-08-26 11:20:45 -0300

    Levels Locker

--- Assets/Scripts/UI/Canvas/LevelLockedCanvas.cs
+++ Assets/Scripts/UI/Canvas/LevelLockedCanvas.cs
@@ -1,18 +1,53 @@
 using System;
+using KeceK.General;
+using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace KeceK.UI
 {
     public class LevelLockedCanvas : MonoBehaviour
     {
+        public static event Action OnIncorrectPassword;
+        
+        [Title("References")]
         [SerializeField] private TMP_InputField _inputField;
+        [SerializeField] private Button _submitButton;
         [SerializeField] private LevelPasswordSO _levelPasswordSO;
-
 
         private void OnEnable()
         {
-            // _inputField.texte
+            _submitButton.onClick.AddListener(() =>
+            {
+                SubmitPassword();
+            });
+        }
+
+        private void SubmitPassword()
+        {
+            if(_inputField.text == _levelPasswordSO.Password)
+            {
+                CorrectPassword();
+            }
+            else
+            {
+                IncorrectPassword();
+            }
+        }
+        
+        private void CorrectPassword()
+        {
+            Debug.Log("Correct Password");
+            _submitButton.interactable = false;
+            _inputField.interactable = false;
+            Loader.LoadNextLevel();
+        }
+
+        private void IncorrectPassword()
+        {
+            Debug.Log("Wrong Password");
+            OnIncorrectPassword?.Invoke();
         }
     }
 }
