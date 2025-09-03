8f802bf12856300b44555cf450c3d711d2f98d15
João Felipe <joaokecek.business@gmail.com>
2025-09-03 20:59:04 -0300

    Level11 Done

---
+++ Assets/Scripts/UI/EndGameCanvas/EndGameCanvas.cs
@@ -0,0 +1,42 @@
+using KeceK.General;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KeceK.UI
+{
+    public class EndGameCanvas : MonoBehaviour
+    {
+        [Title("References")]
+        [SerializeField] [Required]
+        private Button _mainMenuButton;
+        [SerializeField] [Required] 
+        private Button _videoFakeButton;
+        [SerializeField] [Required]
+        private Button _videoRealButton;
+
+        private string _videoFakeURL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+        
+        private string _videoRealURL = "https://www.youtube.com/watch?v=Zi_XLOBDo_Y";
+
+        private void Awake()
+        {
+            _mainMenuButton.gameObject.SetActive(false);
+            _videoRealButton.gameObject.SetActive(false);
+
+            _mainMenuButton.onClick.AddListener(() => { Loader.Load(Loader.Scene.MainMenu); });
+
+            _videoFakeButton.onClick.AddListener(() =>
+            {
+                Application.OpenURL(_videoFakeURL);
+                _videoRealButton.gameObject.SetActive(true);
+            });
+
+            _videoRealButton.onClick.AddListener(() =>
+            {
+                Application.OpenURL(_videoRealURL);
+                _mainMenuButton.gameObject.SetActive(true);
+            });
+    }
+    }
+}
