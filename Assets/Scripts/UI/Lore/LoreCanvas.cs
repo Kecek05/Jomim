bf33921b4e6919a63bf545f6085f57f03bc6daf8
Joao Kecek <joaokecek.business@gmail.com>
2025-09-03 12:28:17 -0300

    Lore Canvas Script

--- Assets/Scripts/UI/Lore/LoreCanvas.cs
+++ Assets/Scripts/UI/Lore/LoreCanvas.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using KeceK.General;
 using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
@@ -9,25 +11,63 @@ namespace KeceK.UI
     public class LoreCanvas : MonoBehaviour
     {
         [Title("References")]
-        [SerializeField] private Button _previousButton;
-        [SerializeField] private Button _nextButton;
-        [SerializeField] private TextMeshProUGUI _contentText;
-        [SerializeField] private TextMeshProUGUI _titleText;
+        [SerializeField] [Required] private Button _previousButton;
+        [SerializeField] [Required] private Button _nextButton;
+        [SerializeField] [Required] private Button _continueButton;
+        [SerializeField] [Required] private TextMeshProUGUI _contentText;
+        [SerializeField] [Required] private TextMeshProUGUI _titleText;
         [Space(5f)]
         [SerializeField] private List<LoreTextSO> _loreTextSOsInOrder;
         
         private int _currentLoreIndex = 0;
         
         private int _maxLoreIndex => _loreTextSOsInOrder.Count - 1;
+
+        private void Awake()
+        {
+            _previousButton.onClick.AddListener(PreviousLore);
+            _nextButton.onClick.AddListener(NextLore);
+            _continueButton.onClick.AddListener(() =>
+            {
+                Loader.Load(Loader.Scene.MainMenu);
+                _continueButton.interactable = false;
+            });
+            _continueButton.gameObject.SetActive(false);
+            
+            UpdateLoreDisplayToCurrentIndex();
+        }
         
+
         private void NextLore()
         {
-            
+            ChangeLoreIndex(_currentLoreIndex + 1);
+            EnableContinueButtonIfAtEnd();
+            UpdateLoreDisplayToCurrentIndex();
         }
 
         private void PreviousLore()
         {
-            
+            ChangeLoreIndex(_currentLoreIndex - 1);
+            UpdateLoreDisplayToCurrentIndex();
+        }
+        
+        private void ChangeLoreIndex(int change)
+        {
+            _currentLoreIndex = Mathf.Clamp(_currentLoreIndex + change, 0, _maxLoreIndex);
+        }
+        
+        private void EnableContinueButtonIfAtEnd()
+        {
+            if (_currentLoreIndex >= _maxLoreIndex)
+            {
+                _continueButton.gameObject.SetActive(true);
+            }
+        }
+
+        private void UpdateLoreDisplayToCurrentIndex()
+        {
+            _contentText.text = _loreTextSOsInOrder[_currentLoreIndex].Content;
+            _titleText.text = _loreTextSOsInOrder[_currentLoreIndex].Title;
         }
     }
 }
